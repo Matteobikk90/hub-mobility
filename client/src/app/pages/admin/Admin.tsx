84f94a461118b7216302d22577f986ffa13c84b4
Matteo Soresini <matteo.soresini@hotmail.it>
2024-09-25 22:36:00 +0200

    grid admin

--- client/src/app/pages/admin/Admin.tsx
+++ client/src/app/pages/admin/Admin.tsx
@@ -114,7 +114,7 @@ export const Admin: React.FC = () => {
       </div>
 
       {/* Display existing cars in the section */}
-      <ul className="grid grid-cols-3 gap-4">
+      <ul className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
         <CarForm
           initialCarData={
             editCarId ? cars.find((car) => car.id === editCarId) : {}
